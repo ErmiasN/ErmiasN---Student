9277b61312b149027f62bb0d1c7214947c648677
ErmiasN <negashe@onid.oregonstate.edu>
2014-10-26 23:52:48 -0700

    final commit

--- Assignment4/src/studentlibrary.js
+++ Assignment4/src/studentlibrary.js
@@ -1,1 +1,57 @@
 //Your library code goes in this file.
+
+function ajaxRequest(url, Type, Parameters){
+  var req = new XMLHttpRequest();
+  if(!req){
+    throw 'Unable to create XMLHttpRequest.';
+  }
+  
+  if(Type === 'GET'){
+    url += '?' + urlStringify(params);
+    req.open('GET', url);
+    req.send();
+  }
+
+  if(Type === 'POST'){
+    req.open('POST', url);
+    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    req.send('Parameters=' + encodeURIComponent(Parameters));
+  }
+  
+  var SuccessObj = {};
+  req.onreadystatechange = function(){
+    if(req.readyState === 4){
+      if(req.status === 200){
+        SucessObj.sucess = true;
+      }
+      else{
+        SuccessObj.sucess = false;
+      }
+      SucessObj.code = req.status;
+      SucessObj.codeDetail = req.statusText;
+      SucessObj.response = req.responseText;
+    }
+  };
+  
+  return SucessObj;
+}
+
+
+function urlStringify(obj){
+  var str = []
+  for(var prop in obj){
+    var s = encodeURIComponent(prop) + '=' + encodeURIComponent(obj[prop]);
+    str.push(s);
+  }
+  return str.join('&');
+}
+
+function localStorageExists(){
+  localStorage.setItem('itWorks', 'Lets test local storage!!');
+  if(localStorage.getItem('itWorks') === 'Lets test local storage!!'){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
