4224698c6da6d7994577d41c7c1dff9904619c14
ErmiasN <negashe@onid.oregonstate.edu>
2014-12-07 12:17:06 -0800

    final commit

---
+++ final-project/goodbye.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+<div>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Goodbye</title>
+		<link href="travel.css"  align="middle" rel="stylesheet" type="text/css">
+	</head>
+	<h1>Goodbye</h1>
+	<img src="goodBye.jpg" alt="Bye">
+	<h2>Lets Travel The World Another Time<h2>
+</body>
+</html>
