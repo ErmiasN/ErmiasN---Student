ed26ad4fcc73e90ae938b0d0687a4625b57a3205
ErmiasN <negashe@onid.oregonstate.edu>
2014-12-07 12:10:17 -0800

    final commit

---
+++ final-project/welcome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+<div>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Welcome</title>
+		<link href="travel.css"  align="middle" rel="stylesheet" type="text/css">
+	</head>
+	<p class="one"><a href="travellogin.php">Travel Login</a></p>
+	<h1>Welcome</h1>
+	<img src="WhiteSand.jpg" alt="Beach">
+	<h2>Lets Travel The World<h2>
+</body>
+</html>
